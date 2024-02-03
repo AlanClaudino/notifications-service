a1d1ea540bf550ceebc57cda89e6963de3e5de9c
Alan Claudino <alanrodrigoclaudino@gmail.com>
2024-02-03 17:11:18 -0300

    implementing cancel notification use case and test

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -6,6 +6,26 @@ export class InMemoryNotificationsRepository
 {
   public notifications: Notification[] = []
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    )
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    )
+
+    this.notifications[notificationIndex] = notification
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification)
   }

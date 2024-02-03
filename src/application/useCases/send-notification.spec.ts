dc2ee6abfa25feb5dc891fd19bf9a685153b8648
Alan Claudino <alanrodrigoclaudino@gmail.com>
2024-02-03 13:13:13 -0300

    implementing http module and first route

---
+++ src/application/useCases/send-notification.spec.ts
@@ -0,0 +1,18 @@
+import { SendNotification } from './send-notification'
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notification-repository'
+
+describe('Send notification', () => {
+  it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository()
+    const sendNotification = new SendNotification(notificationsRepository)
+
+    const { notification } = await sendNotification.execute({
+      recipientId: 'example-recipient-id',
+      category: 'social',
+      content: 'This is a notification',
+    })
+
+    expect(notificationsRepository.notifications).toHaveLength(1)
+    expect(notificationsRepository.notifications[0]).toEqual(notification)
+  })
+})
